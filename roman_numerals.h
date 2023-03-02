4c0de074fea6715d713b9e1615e341eb79b5b4cc
Simon Stockton <61173796+simonstockton763@users.noreply.github.com>
2023-03-02 16:25:35 -0600

    Create roman_numerals.h

---
+++ roman_numerals.h
@@ -0,0 +1,15 @@
+#if !defined(ROMAN_NUMERALS_H)
+#define ROMAN_NUMERALS_H
+#include <string>
+namespace roman_numerals {
+  std::string convert(int);
+  std::string penturies(int);
+  std::string centuries(int);
+  std::string pentecostades(int);
+  std::string decades(int);
+  std::string pentads(int);
+  std::string singles(int);
+
+}  // namespace roman_numerals
+
+#endif // ROMAN_NUMERALS_H
